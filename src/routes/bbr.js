7e3ef5893ff1ed8df0bc6ab7390cf67ee30df01a
SmillaNgit config --global user.email smilla.oh@gmail.com      git clone https://github.com/sylvestercbs/progeksamen.gitcd progeksamengit config --global user.name SmillaOH <smilla.oh@gmail.com>
2026-04-28 11:49:22 +0200

    Tilføj fejlhåndtering i BBR route

--- src/routes/bbr.js
+++ src/routes/bbr.js
@@ -2,28 +2,32 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const { adresseId } = req.query;
+  try {
+    const { adresseId } = req.query;
+    if (!adresseId) {
+      return res.status(400).json({ error: 'Mangler adresseId' });
+    }
 
-  if (!adresseId) {
-    return res.status(400).json({ error: 'Mangler adresseId' });
+    const url = new URL('https://services.datafordeler.dk/BBR/BBRPublic/1/REST/enhed');
+    url.searchParams.set('username', process.env.BBR_USERNAME);
+    url.searchParams.set('password', process.env.BBR_PASSWORD);
+    url.searchParams.set('AdresseIdentificerer', adresseId);
+    url.searchParams.set('format', 'JSON');
+
+    const response = await fetch(url.toString());
+    const text = await response.text();
+    console.log("BBR svar:", text);
+
+    const data = JSON.parse(text);
+    const enhed = data[0] || {};
+    const resultat = {
+      boligareal:     enhed.enh026EnhedensSamledeAreal || null,
+      antalVaerelser: enhed['enh031AntalVærelser']      || null,
+    };
+    res.json(resultat);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-
-  const url = new URL('https://services.datafordeler.dk/BBR/BBRPublic/1/REST/enhed');
-  url.searchParams.set('username', process.env.BBR_USERNAME);
-  url.searchParams.set('password', process.env.BBR_PASSWORD);
-  url.searchParams.set('AdresseIdentificerer', adresseId);
-  url.searchParams.set('format', 'JSON');
-
-  const response = await fetch(url.toString());
-  const data = await response.json();
-
-  const enhed = data[0] || {};
-  const resultat = {
-    boligareal:     enhed.enh026EnhedensSamledeAreal  || null,
-    antalVaerelser: enhed['enh031AntalVærelser']       || null,
-  };
-
-  res.json(resultat);
 });
 
 module.exports = router;
